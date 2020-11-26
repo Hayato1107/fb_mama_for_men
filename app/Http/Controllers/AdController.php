ef276a814ed09c170728dfff1bb03e172dc4ec58
EC2 Default User <ec2-user@ip-172-31-28-192.us-east-2.compute.internal>
2020-11-26 19:17:05 +0000

    Mm

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,8 +10,8 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://preaf.jp/pa.do?s=v04031&o=45765&guid=ON'); //トークウィズ - Preaf    
+            return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
         }
-        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16216'); //トリトモ - fam
+        return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
     }
 }
