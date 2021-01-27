9971d56a9fa3a5a426c22c9c18f34cecb333a905
EC2 Default User <ec2-user@ip-172-31-28-192.us-east-2.compute.internal>
2021-01-27 07:17:48 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,8 +10,9 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
+            // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
+            return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
         }
-        return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
+        return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
     }
 }
