44824d3c2d44d05d305a7b6ae4b36112a15625e8
EC2 Default User <ec2-user@ip-172-31-28-192.us-east-2.compute.internal>
2021-02-16 19:24:49 +0000

    M

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 //獲得用
-Route::get('tp', function () {
+Route::get('ins1', function () {
     return view('fb.check'); //提出用
     // return view('fb.pp'); //パパ活訴求 - 獲得用(3)
     // return view('fb.newLive'); //獲得用(3)
