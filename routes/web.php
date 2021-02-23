7bfb605a9d1fd89771d8eaf413523a7a5ccef460
EC2 Default User <ec2-user@ip-172-31-28-192.us-east-2.compute.internal>
2021-02-23 19:17:28 +0000

    M

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 //獲得用
 Route::get('ins1', function () {
-    // return view('fb.check'); //提出用
-    return view('fb.newLive'); //獲得用(3)
+    return view('fb.check'); //提出用
+    // return view('fb.newLive'); //獲得用(3)
     // return view('fb.pp'); //パパ活訴求 - 獲得用(3)
     
     // return view('fb.job'); //獲得用(2)
